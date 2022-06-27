ae5f57021ccf0b41921e22869f8cac8b9c865ccb
aaswani365 <107995802+aaswani365@users.noreply.github.com>
2022-06-27 19:40:41 +0530

    Delete project directory

--- project/scoop/export_data_mysql.sh
+++
@@ -1,6 +0,0 @@
-sqoop export \
---connect jdbc:mysql://localhost:3306/project?useSSL=false \
---username root --password-file file:///home/saif/project/env/password.pwd \
---table daily_transactions_data -m 1 \
---export-dir /user/saif/project/datasets
-
--- project/scoop/import_data_to_hdfs.sh
+++
@@ -1,6 +0,0 @@
-sqoop job --create fresh_day_import_job -- import \
---connect jdbc:mysql://localhost:3306/project?useSSL=False \
---username root --password-file file:///home/saif/project/env/password.pwd \
---table daily_transactions_data -m 1 \
---target-dir /user/saif/project/daily_data \
---incremental append --check-column custid  --last-value 0
